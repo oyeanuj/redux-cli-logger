c8e4e6cf6550d9a60bee77f921abb8e0005ff915
anuj <anujnijhawan@gmail.com>
2017-10-03 13:01:16 -0700

    More formatting nitpicks

--- src/index.js
+++ src/index.js
@@ -58,9 +58,9 @@ export default function createCLILogger (options) {
     const message = `\n${downArrow} Action :: ${chalk.bold(action.type)} @ ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
 
     const output = `${chalk[messageColor](message)}\n` +
-      `  ${chalk[prevColor](chalk.bold.underline.bgWhite('PREVIOUS STATE'), `\n${prevState}`)}` +
-      `  ${chalk[actionColor](chalk.bold.underline.bgWhite('ACTION'), `\n${actionDisplay}`)}` +
-      `  ${chalk[nextColor](chalk.bold.underline.bgWhite('NEXT STATE'), `\n${nextState}`)}`
+      `  ${chalk[prevColor](chalk.bold.underline('PREVIOUS STATE'), `\n${prevState}`)}` +
+      `  ${chalk[actionColor](chalk.bold.underline('ACTION'), `\n${actionDisplay}`)}` +
+      `  ${chalk[nextColor](chalk.bold.underline('NEXT STATE'), `\n${nextState}`)}`
 
     console.log(output)
     return returnValue
